72669d6e9345d5b9bb13270e98c756090848723f
jgiors <jgiors@yahoo.com>
2025-02-28 21:31:22 -0800

    use translate and scale to draw circle

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 function love.draw()
     love.graphics.clear(0.4, 0.1, 0.3)
     love.graphics.setColor(0,0,0)
-    love.graphics.circle("fill", 100, 100, 50)
+    love.graphics.translate(400, 200)
+    love.graphics.scale(100,100)
+    love.graphics.circle("fill", 0, 0, 0.5)
 end
