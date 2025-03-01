b693cb07f6ff70daeae16a9b4c0f35c779da8cf5
jgiors <jgiors@yahoo.com>
2025-02-28 20:55:41 -0800

    added conf.lua and empty main.lua

---
+++ conf.lua
@@ -0,0 +1,7 @@
+--LOVE configuration file. See https://love2d.org/wiki/Config_Files
+function love.conf(t)
+    t.identity = "sandbox"
+    t.console = false
+    t.window.width = 800
+    t.window.height = 600
+end
