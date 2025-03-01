5641077ce7701da10740a0373defb4ed9ea0db96
jgiors <jgiors@yahoo.com>
2025-02-28 23:25:19 -0800

    drawing multiple circles using view transform, push, world transform, pop

--- main.lua
+++ main.lua
@@ -1,7 +1,24 @@
+local graphics = love.graphics
+local push, pop, translate, scale, circle = graphics.push, graphics.pop, graphics.translate, graphics.scale, graphics.circle
+
+local function drawCircle(x,y, size)
+    push()
+    translate(x,y)
+    scale(size,size)
+    circle("fill", 0, 0, 0.5, 24)
+    pop()
+end
+
 function love.draw()
-    love.graphics.clear(0.4, 0.1, 0.3)
-    love.graphics.setColor(0,0,0)
-    love.graphics.translate(400, 200)
-    love.graphics.scale(100,100)
-    love.graphics.circle("fill", 0, 0, 0.5)
+    graphics.clear(0.4, 0.1, 0.3)
+
+    --set view transform (offset and world-to-screen scale)
+    translate(100, 100)
+    scale(100,100)
+
+    graphics.setColor(0,0,0)   --draw all circles in black
+
+    drawCircle(4,2, 1)
+    drawCircle(1,1, 1.5)
+    drawCircle(1.5, 4, 0.6)
 end
